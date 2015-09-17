75d84680e93e56501e3bb82ddb785e855a350de2
Allain Lalonde <allain.lalonde@gmail.com>
2015-09-17 12:01:33 -0400

    better errors

--- bin/actions/register.js
+++ bin/actions/register.js
@@ -3,14 +3,16 @@ var npm = require('npm');
 var temp = require('temp');
 
 module.exports = function (argv) {
+  var serverHost = argv.host || '127.0.0.1';
+  var serverPort = argv.port || 2020;
   var service = new ServicifyService({
-    host: argv.host || '127.0.0.1',
-    port: argv.port || 2020
+    host: serverHost,
+    port: serverPort
   });
 
   var targetName = argv._[1];
 
-  service.register(argv._[1]).then(function (r) {
+  service.register(targetName).then(function (r) {
     console.log('registered local', r.name + '@' + r.version, ' with ', r.server.host + ':' + r.server.host);
   }, function (err) {
     if (err.code !== 'MODULE_NOT_FOUND') throw err;
@@ -21,19 +23,21 @@ module.exports = function (argv) {
 
         process.chdir(npm.config.get('prefix'));
 
-        service.register(argv._[1]).then(function (r) {
+        service.register(targetName).then(function (r) {
           console.log('registered global', r.name + '@' + r.version, ' with ', r.server.host + ':' + r.server.host);
         }, reject);
       });
     });
   }).catch(function (err) {
     if (err.code === 'MODULE_NOT_FOUND') {
-      console.error('could not resolve package:', argv._[1]);
+      console.error('could not resolve package:', targetName);
+    } else if (err.code === 'ECONNREFUSED') {
+      console.error('unable to connect to server at', serverHost + ':' + serverPort)
     } else {
       console.error(err.message);
       console.error(err);
     }
+
+    process.exit(1);
   });
-
-
 };
